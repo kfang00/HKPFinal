d74029ab9e31dcb61b4af4b2b712102864e24f52
Kayla Fang <kayfang182@gmail.com>
2020-10-11 00:12:17 -0400

    made file

---
+++ Shopping/Shopping/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shopping
+//
+//  Created by Kayla Fang on 10/11/20.
+//  Copyright © 2020 Kayla Fang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
